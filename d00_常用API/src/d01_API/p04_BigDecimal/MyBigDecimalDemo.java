597071e7c7dd9ae45d75ae4048467d90af5594f1
lwqq <hehe@hehe.com>
2023-05-30 13:43:55 +0800

    1

--- d00_常用API/src/d01_API/p04_BigDecimal/MyBigDecimalDemo.java
+++ d00_常用API/src/d01_API/p04_BigDecimal/MyBigDecimalDemo.java
@@ -5,11 +5,11 @@ import java.math.BigDecimal;
 public class MyBigDecimalDemo {
     public static void main(String[] args) {
         System.out.println("浮点数直接计算不够精确");
-        System.out.println(10.0 / 3.0);          //不够精确
-        System.out.println(0.1+0.2);             //不够精确
+        System.out.println("10.0 / 3.0 =" + 10.0 / 3.0); // 不够精确
+        System.out.println("0.1 + 0.2=" + 0.1 + 0.2); // 不够精确
         System.out.println("-----------------------------------------");
 
-        BigDecimal bd1 = new BigDecimal("0.1"); //字符串的构造比直接传数字更精确
+        BigDecimal bd1 = new BigDecimal("0.1"); // 字符串的构造比直接传数字更精确
         BigDecimal bd2 = new BigDecimal("0.2");
 
         System.out.println("和为" + bd1.add(bd2));
@@ -18,22 +18,20 @@ public class MyBigDecimalDemo {
 
         System.out.println("积为" + bd1.multiply(bd2));
 
-        System.out.println("商为"+bd1.divide(bd2));
+        System.out.println("商为" + bd1.divide(bd2));
 
         System.out.println("-----------------------------------------------------");
         BigDecimal bd3 = new BigDecimal("10");
         BigDecimal bd4 = new BigDecimal("3");
 
-        //System.out.println(bd3.divide(bd4));    //无限不循环不能精确，所以会报错
-
-        BigDecimal divide = bd3.divide(bd4, 2, BigDecimal.ROUND_HALF_UP); //45
-        BigDecimal divide2 = bd3.divide(bd4, 2, BigDecimal.ROUND_UP);     //进一
-        BigDecimal divide3 = bd3.divide(bd4, 2, BigDecimal.ROUND_FLOOR);  //去尾
+        // System.out.println(bd3.divide(bd4)); //无限小数不能精确，所以会报错
+  
+        BigDecimal divide = bd3.divide(bd4, 2, BigDecimal.ROUND_HALF_UP); // 45
+        BigDecimal divide2 = bd3.divide(bd4, 2, BigDecimal.ROUND_UP); // 进一
+        BigDecimal divide3 = bd3.divide(bd4, 2, BigDecimal.ROUND_FLOOR); // 去尾
         System.out.println(divide);
         System.out.println(divide2);
         System.out.println(divide3);
-
-
 
     }
 }
--- d03_字符串String/src/com/Contains.java
+++
@@ -1,8 +0,0 @@
-package com;
-
-public class Contains {
-    public static void main(String[] args) {
-        String s="aaabbbccc";
-        System.out.println(s.contains("abbbc"));
-    }
-}
